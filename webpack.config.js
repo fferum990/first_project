d51d05b61f07624bbc3448d25e4f8b5db45b4419
fferum990 <vfndtq2001@gmail.com>
2021-10-17 05:08:21 +0600

    дошел до бога фронтенда. теперь точно анимация работает

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     mode: 'development',
     entry: {
         game:'./js/game.js',
-        post:'./js/post.js'
+        post:'./js/post.js',
     },
     output:{
         path: path.resolve(__dirname, 'dist'),
